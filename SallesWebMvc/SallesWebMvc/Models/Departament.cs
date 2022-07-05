40a122c228a5ba166d45942d64bf2ef18177d393
leila <leeilacontell@gmail.com>
2022-07-05 19:14:30 -0300

    Other entities and second migration

--- SallesWebMvc/SallesWebMvc/Models/Departament.cs
+++ SallesWebMvc/SallesWebMvc/Models/Departament.cs
@@ -1,8 +1,38 @@
-﻿namespace SallesWebMvc.Models
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SallesWebMvc.Models
+
 {
     public class Departament
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public ICollection<Seller> Sellers { get; set; } = new List<Seller>();
+
+        public Departament()
+        {
+
+        }
+        public Departament(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        public void AddSeller(Seller seller)
+        {
+            Sellers.Add(seller);
+        }
+
+        public double TotalSales(DateTime initial, DateTime final)
+        {
+            // Passa uma expressão labda que filtra as vendas as penas nessas datas
+            // para cada vendedores da lista chamando o total de vendas do vendedor no periodo inicial e final
+            // e faz uma soma de todas as vendas do departamento
+
+            return Sellers.Sum(seller => seller.TotalSales(initial, final));
+        }
     }
 }
