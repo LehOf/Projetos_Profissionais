6502bfa9ef20b5876eded2c86d1e16de4e0904e1
leila <leeilacontell@gmail.com>
2022-07-05 23:51:35 -0300

    SellerService and basic FindAll

---
+++ SallesWebMvc/SallesWebMvc/Services/SellerService.cs
@@ -0,0 +1,22 @@
+﻿using SallesWebMvc.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SallesWebMvc.Services
+{
+    public class SellerService
+    {
+        private readonly SallesWebMvcContext _context; 
+
+        public SellerService(SallesWebMvcContext context)        {
+            _context = context;
+        }
+
+        //Retorna uma lista com todos os vendedores do banco de dados
+
+        public List<Seller> FindAll()
+        {
+            return _context.Seller.ToList();
+        }
+    }
+}
