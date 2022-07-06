831161739f3d98a2332aabc513f09cbf7cc65315
leila <leeilacontell@gmail.com>
2022-07-06 15:34:47 -0300

    Simple Create form

--- SallesWebMvc/SallesWebMvc/Controllers/SellersController.cs
+++ SallesWebMvc/SallesWebMvc/Controllers/SellersController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SallesWebMvc.Models;
 using SallesWebMvc.Services;
 
 namespace SallesWebMvc.Controllers
@@ -16,6 +17,19 @@ namespace SallesWebMvc.Controllers
         {
             var list = _sellerService.FindAll();
             return View(list);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Seller seller)
+        {
+            _sellerService.Insert(seller);
+            return RedirectToAction(nameof(Index));
         }
     }
 }
