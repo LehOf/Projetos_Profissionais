a1342d0d56d1d68b87955d15a10cb4a975b96a70
leila <leeilacontell@gmail.com>
2022-07-07 16:26:04 -0300

    Foreign key not null (referential integrity)

--- SallesWebMvc/SallesWebMvc/Services/SellerService.cs
+++ SallesWebMvc/SallesWebMvc/Services/SellerService.cs
@@ -21,6 +21,7 @@ namespace SallesWebMvc.Services
 
         public void Insert(Seller obj)
         {
+            obj.Departament = _context.Departament.First();
             _context.Add(obj);
             _context.SaveChanges();
         }
